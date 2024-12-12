de5c3c99e1945a779aa3c6bc7807a7dee6a22f02
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-12-12 22:23:33 +0200

    fix error with coding UTF16LE and UTF8 with BOM - change in manually to UTF8

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,11 +1,7 @@
 function Welcome() {
     return (
         <div className='welcome'>
-            {/* <div className='welcome__image welcome__image--red'>11</div> */}
-            <span>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas,
-                fugiat!
-            </span>
+            <div className='welcome__image'></div>
         </div>
     );
 }
