c730443727df7d2999840bc8d75a79ef76a6e6d1
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-12-18 13:35:43 +0200

    add NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,25 +1,43 @@
+import React, { useState, useEffect } from 'react';
 function NotFound() {
+    const [isVulgare, setIsVulgare] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsVulgare(true);
+            setTimeout(() => {
+                setIsVulgare(false);
+            }, 7000);
+        }, 5000);
+    }, []);
+
     return (
         <div className='sentence'>
             <img
                 className='sentence__image'
-                src='/src/assets/image/book/p62.webp'
-                alt='book title'
+                src={
+                    !isVulgare
+                        ? '/src/assets/image/book/p62.webp'
+                        : '/src/assets/image/book/p0.webp'
+                }
+                alt='not found'
             />
 
             <div className='home_quotes'>
-                <blockquote className='sentence__quote'>
-                    <span className='sentence__quote__text text__header'>
-                        Не найдено.
-                    </span>
-                </blockquote>
-                {/* <blockquote className='sentence__quote '>
-                    <span className='sentence__quote__text text__small'>
-                        Ибо сказано:
+                {!isVulgare ? (
+                    <blockquote className='sentence__quote'>
+                        <span className='sentence__quote__text text__base'>
+                            Не найдено.
+                        </span>
+                    </blockquote>
+                ) : (
+                    <blockquote className='sentence__quote '>
+                        <pre className='sentence__quote__text text__base'>
                             Хуйня случается.
-                    </span>
-                    <cite className='text__small'>...</cite>
-                </blockquote> */}
+                        </pre>
+                        <cite className='text__small'>...</cite>
+                    </blockquote>
+                )}
             </div>
         </div>
     );
