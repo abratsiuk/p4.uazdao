e2239f9db2e14d055716649f9f054cd0aa7aeb48
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-12-20 15:27:38 +0200

    add arrows components

--- src/component/AbstractArrow.jsx
+++ src/component/AbstractArrow.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { isPointInTriangle } from '../services/geometry';
+import { isPointInRectangle } from '../services/geometry';
 
 function AbstractArrow({
     children,
@@ -31,10 +31,10 @@ function AbstractArrow({
             // Координаты клика относительно target
             const clickX = el.clientX - rect.x;
             const clickY = el.clientY - rect.y;
-            // console.log(
-            //     'Координаты клика относительно target',
-            //     `(${Math.round(clickX)}, ${Math.round(clickY)})`
-            // );
+            console.log(
+                'Координаты клика относительно target',
+                `(${Math.round(clickX)}, ${Math.round(clickY)})`
+            );
 
             //settings for ::before, ::after
             //border: 20px;
@@ -46,70 +46,16 @@ function AbstractArrow({
             //::before, ::after { top: calc(50% - 20px);
             const top = Math.round(rect.height / 2 - border);
 
-            //precision for click: +- 10px in coordinate
-            const precision = 20;
+            const x1 =
+                direction === 'left' ? left : rect.width - right - 2 * border;
+            const x2 =
+                direction === 'left' ? left + 2 * border : rect.width - right;
+            const y1 = top;
+            const y2 = top + 2 * border;
 
-            let x1 = 0,
-                y1 = 0,
-                x2 = 0,
-                y2 = 0,
-                x3 = 0,
-                y3 = 0;
-            if (direction === 'left') {
-                x1 = border + left - 2 * precision;
-                x2 = border + border + left + precision;
-                x3 = border + border + left + precision;
-                y1 = Math.round(top + border);
-                y2 = Math.round(top + border - border - precision);
-                y3 = Math.round(top + border + border + precision);
-                // console.log(
-                //     `координаты точек левого треугольника с заступом в ${precision}:
-                // (${border + left - 2 * precision}, ${Math.round(
-                //     border - border + rect.height / 2
-                //     )}),
-                // (${border + border + left + precision}, ${Math.round(
-                //     border - border + rect.height / 2 - border - precision
-                //     )}),
-                // (${border + border + left + precision}, ${Math.round(
-                //     border - border + rect.height / 2 + border + precision
-                //     )})`
-                // );
-            }
+            console.log(`rectangle:(${x1},${y1}),(${x2},${y2})`);
 
-            if (direction === 'right') {
-                x1 = rect.width - border - border - right - precision;
-                x2 = rect.width - border - right + 2 * precision;
-                x3 = rect.width - border - border - right - precision;
-                y1 = Math.round(top + border - border - precision);
-                y2 = Math.round(top + border);
-                y3 = Math.round(top + border + border + precision);
-                // console.log(
-                //     `координаты точек правого треугольника с заступом в ${precision}:
-                // (${Math.round(
-                //     rect.width - border - border - right - precision
-                // )}, ${Math.round(
-                //         Math.round(
-                //             border - border + rect.height / 2 - border - precision
-                //         )
-                //     )}),
-                // (${Math.round(
-                //     rect.width - border - right + 2 * precision
-                // )}, ${Math.round(
-                //         Math.round(border - border + rect.height / 2)
-                //     )},
-                // (${Math.round(
-                //     rect.width - border - border - right - precision
-                // )}, ${Math.round(
-                //         Math.round(
-                //             border - border + rect.height / 2 + border + precision
-                //         )
-                //     )})`
-                // );
-            }
-
-            if (
-                isPointInTriangle(clickX, clickY, x1, y1, x2, y2, x3, y3, 0.3)
-            ) {
+            if (isPointInRectangle(clickX, clickY, x1, y1, x2, y2)) {
                 console.log('Arrow clicked');
                 onClick();
             }
--- src/services/geometry.js
+++ src/services/geometry.js
@@ -1,3 +1,11 @@
+export const isPointInRectangle = (px, py, x1, y1, x2, y2) => {
+    const minX = Math.min(x1, x2);
+    const maxX = Math.max(x1, x2);
+    const minY = Math.min(y1, y2);
+    const maxY = Math.max(y1, y2);
+    return px >= minX && px <= maxX && py >= minY && py <= maxY;
+};
+
 export const isPointInTriangle = (
     px,
     py,
