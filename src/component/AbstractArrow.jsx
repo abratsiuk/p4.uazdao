130d9907e62e645cfd4e2f8cd45d15a79aba2594
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-12-20 15:09:37 +0200

    add arrows components

--- src/component/AbstractArrow.jsx
+++ src/component/AbstractArrow.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import { isPointInTriangle } from '../services/geometry';
+
 function AbstractArrow({
     children,
     className,
@@ -6,66 +9,110 @@ function AbstractArrow({
 }) {
     const classes = `${className} arrow arrow--${direction}`;
     const onHandleClick = (el) => {
-        console.log(`Arrow ${direction} clicked`);
+        // console.log(`Arrow ${direction} clicked`);
 
         const targetClassNames = el.target.classList.value;
         if (classes === targetClassNames) {
             const rect = el.target.getBoundingClientRect();
-            //Координаты target
-            console.log(
-                'Координаты target:',
-                `(${Math.round(rect.x)}, ${Math.round(rect.y)})`
-            );
-            // Размеры target
-            console.log(
-                'Размеры target:',
-                Math.round(rect.width),
-                Math.round(rect.height)
-            );
-            // Координаты клика
-            console.log('Координаты клика', `(${el.clientX}, ${el.clientY})`);
+            // //Координаты target
+            // console.log(
+            //     'Координаты target:',
+            //     `(${Math.round(rect.x)}, ${Math.round(rect.y)})`
+            // );
+            // // Размеры target
+            // console.log(
+            //     'Размеры target:',
+            //     Math.round(rect.width),
+            //     Math.round(rect.height)
+            // );
+            // // Координаты клика
+            // console.log('Координаты клика', `(${el.clientX}, ${el.clientY})`);
 
             // Координаты клика относительно target
             const clickX = el.clientX - rect.x;
             const clickY = el.clientY - rect.y;
-            console.log(
-                'Координаты клика относительно target',
-                `(${Math.round(clickX)}, ${Math.round(clickY)})`
-            );
+            // console.log(
+            //     'Координаты клика относительно target',
+            //     `(${Math.round(clickX)}, ${Math.round(clickY)})`
+            // );
 
-            const precision = 10;
+            //settings for ::before, ::after
+            //border: 20px;
+            const border = 20;
+            // ::before { left: 20px;
+            const left = 20;
+            // ::after { right: 0;
+            const right = 0;
+            //::before, ::after { top: calc(50% - 20px);
+            const top = Math.round(rect.height / 2 - border);
 
+            //precision for click: +- 10px in coordinate
+            const precision = 20;
+
+            let x1 = 0,
+                y1 = 0,
+                x2 = 0,
+                y2 = 0,
+                x3 = 0,
+                y3 = 0;
             if (direction === 'left') {
-                console.log(
-                    `координаты точек левого треугольника с заступом в ${precision}:
-                (${20 + 20 - 2 * precision}, ${Math.round(
-                        +20 - 20 + rect.height / 2
-                    )}), 
-                (${20 + 20 + 20 + precision}, ${Math.round(
-                        +20 - 20 + rect.height / 2 - 20 - precision
-                    )}), 
-                (${20 + 20 + 20 + precision}, ${Math.round(
-                        +20 - 20 + rect.height / 2 + 20 + precision
-                    )})`
-                );
+                x1 = border + left - 2 * precision;
+                x2 = border + border + left + precision;
+                x3 = border + border + left + precision;
+                y1 = Math.round(top + border);
+                y2 = Math.round(top + border - border - precision);
+                y3 = Math.round(top + border + border + precision);
+                // console.log(
+                //     `координаты точек левого треугольника с заступом в ${precision}:
+                // (${border + left - 2 * precision}, ${Math.round(
+                //     border - border + rect.height / 2
+                //     )}),
+                // (${border + border + left + precision}, ${Math.round(
+                //     border - border + rect.height / 2 - border - precision
+                //     )}),
+                // (${border + border + left + precision}, ${Math.round(
+                //     border - border + rect.height / 2 + border + precision
+                //     )})`
+                // );
             }
 
             if (direction === 'right') {
-                console.log(
-                    `координаты точек правого треугольника с заступом в ${precision}: 
-                (${Math.round(rect.width - 20 - 20 - precision)}, ${Math.round(
-                        Math.round(+20 - 20 + rect.height / 2 - 20 - precision)
-                    )}), 
-                (${Math.round(rect.width - 20 + 2 * precision)}, ${Math.round(
-                        Math.round(+20 - 20 + rect.height / 2)
-                    )}, 
-                (${Math.round(rect.width - 20 - 20 - precision)}, ${Math.round(
-                        Math.round(+20 - 20 + rect.height / 2 + 20 + precision)
-                    )})`
-                );
+                x1 = rect.width - border - border - right - precision;
+                x2 = rect.width - border - right + 2 * precision;
+                x3 = rect.width - border - border - right - precision;
+                y1 = Math.round(top + border - border - precision);
+                y2 = Math.round(top + border);
+                y3 = Math.round(top + border + border + precision);
+                // console.log(
+                //     `координаты точек правого треугольника с заступом в ${precision}:
+                // (${Math.round(
+                //     rect.width - border - border - right - precision
+                // )}, ${Math.round(
+                //         Math.round(
+                //             border - border + rect.height / 2 - border - precision
+                //         )
+                //     )}),
+                // (${Math.round(
+                //     rect.width - border - right + 2 * precision
+                // )}, ${Math.round(
+                //         Math.round(border - border + rect.height / 2)
+                //     )},
+                // (${Math.round(
+                //     rect.width - border - border - right - precision
+                // )}, ${Math.round(
+                //         Math.round(
+                //             border - border + rect.height / 2 + border + precision
+                //         )
+                //     )})`
+                // );
             }
 
-            onClick();
+            if (
+                isPointInTriangle(clickX, clickY, x1, y1, x2, y2, x3, y3, 0.3)
+            ) {
+                console.log('Arrow clicked');
+                onClick();
+            }
         }
     };
     return (
---
+++ src/services/geometry.js
@@ -0,0 +1,30 @@
+export const isPointInTriangle = (
+    px,
+    py,
+    x1,
+    y1,
+    x2,
+    y2,
+    x3,
+    y3,
+    tolerance = 0.1
+) => {
+    // calculate the area of the triangle
+    function calculateArea(x1, y1, x2, y2, x3, y3) {
+        return Math.abs((x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2);
+    }
+
+    // full triangle area
+    const A = calculateArea(x1, y1, x2, y2, x3, y3);
+
+    // areas of subtriangles
+    const A1 = calculateArea(px, py, x2, y2, x3, y3);
+    const A2 = calculateArea(x1, y1, px, py, x3, y3);
+    const A3 = calculateArea(x1, y1, x2, y2, px, py);
+    const sumAreas = A1 + A2 + A3;
+
+    // check is the point in the triangle
+    const maxArea = Math.max(A, sumAreas);
+    const difference = Math.abs(A - sumAreas);
+    return difference <= maxArea * tolerance;
+};
