3977887f5779abe3eceb469529f124f30879188f
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-12-24 19:50:16 +0200

    refactoring css

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import './styles/_typography.css';
-import './styles/_debug.css';
 
 import { App } from './components/App';
 
