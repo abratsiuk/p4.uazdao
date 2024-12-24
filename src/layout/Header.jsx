f2d1630662f49ed3599ea52c5e1059f47c6408d3
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-12-24 15:54:16 +0200

    refactoring css: header, menu active and hover

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,83 +1,80 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 function Header() {
     return (
         <header
             id='header'
-            className='header text-base'
+            className='header'
         >
-            <div className='header__text  great-vibes-regular '>
-                <Link
-                    to='/'
-                    className='link'
-                >
-                    Уаздао
-                </Link>
-            </div>
-            <nav className='header__nav'>
-                <ul className='menu'>
-                    <li>
-                        <Link
-                            to='/sentence'
-                            className='link text-action'
-                        >
-                            изречение
-                        </Link>
-                    </li>
-                    <li>
-                        {' '}
-                        <Link
-                            to='/author'
-                            className='link text-action'
-                        >
-                            Павел Иевлев
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            to='/book-uazdao'
-                            className='link text-action'
-                        >
-                            версия 1
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            to='/book-multiverse'
-                            className='link text-action'
-                        >
-                            версия 2
-                        </Link>
-                    </li>{' '}
-                    <li>
-                        <Link
-                            to='/book-mizantrop'
-                            className='link text-action'
-                        >
-                            версия 3
-                        </Link>
-                    </li>
-                    {/* <li>
-                        {' '}
-                        <Link
-                            to='/goodgrem'
-                            className='link text-action'
-                        >
-                            добрый грём
-                        </Link>
-                    </li>
-
-                    <li>
-                        {' '}
-                        <Link
-                            to='/parts'
-                            className='link text-action'
-                        >
-                            запчасти
-                        </Link>
-                    </li> */}
-                </ul>
-            </nav>
+            <Link
+                to='/'
+                className='header__logo'
+            >
+                Уаздао
+            </Link>
+            <ul className='header__menu'>
+                <li>
+                    <NavLink
+                        to='/home'
+                        className='link'
+                        exact
+                        activeClassName='link-active'
+                    >
+                        главная
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/sentence'
+                        className='link'
+                        exact
+                        activeClassName='link-active'
+                    >
+                        изречение
+                    </NavLink>
+                </li>
+                <li>
+                    {' '}
+                    <NavLink
+                        to='/author'
+                        className='link text-action'
+                        exact
+                        activeClassName='link-active'
+                    >
+                        Павел Иевлев
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/book-uazdao'
+                        className='link text-action'
+                        exact
+                        activeClassName='link-active'
+                    >
+                        версия 1
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/book-multiverse'
+                        className='link text-action'
+                        exact
+                        activeClassName='link-active'
+                    >
+                        версия 2
+                    </NavLink>
+                </li>{' '}
+                <li>
+                    <NavLink
+                        to='/book-mizantrop'
+                        className='link text-action'
+                        exact
+                        activeClassName='link-active'
+                    >
+                        версия 3
+                    </NavLink>
+                </li>
+            </ul>
         </header>
     );
 }
